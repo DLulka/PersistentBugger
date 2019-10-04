65e187913609d1c405945940c6efdd15fab67e42
daniel.lulka <daniel.lulka@bbva.com>
2019-10-04 09:43:02 -0500

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(Main.persistence(39));
+	System.out.println(Main.persistence(999));
     }
     public static int persistence(long n) {
-        // your code
+     long multi = 1, ans = 0;
+     while(n >= 10){
+         while(n > 0) {
+             multi = multi * (n % 10);
+             n = n / 10 ;
+         }
+         if(multi >= 10)
+             n = multi;
+         multi = 1;
+         ans ++;
+     }
+        return (int) ans;
     }
 }
